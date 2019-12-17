1f0002a383e93a0c8895d5e54c43f3bd1dc0eb7f
Marvin6262 <marvinaj12@gmail.com>
2019-12-17 15:02:33 +0700

    New Update

---
+++ Test Suites/Delete Akun Bank/Delete Akun Bank.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Delete Akun Bank</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8a1f1254-214f-4045-9767-aa3c6383f574</testSuiteGuid>
+   <testCaseLink>
+      <guid>51cd3268-5ad3-4978-9f13-68d8c694fd19</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Delete Akun Bank/Delete Akun Bank</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>76948677-241e-4c7c-b679-687bae211e8d</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Delete Akun Bank/Delete Akun Bank</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>76948677-241e-4c7c-b679-687bae211e8d</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>NoRek</value>
+         <variableId>2d31fd83-aa37-4014-831b-7b2602a87c81</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
