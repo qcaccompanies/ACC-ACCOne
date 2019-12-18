8798b08c1f7b704dc85d7dd0ea71dd68c165a792
= <=>
2019-12-18 15:01:59 +0700

    Ubah Email - ATS-13

--- Scripts/Daftar Akun tests/Daftar Akun - Main flow - ATS-14/Script1576653828415.groovy
+++ Scripts/Daftar Akun tests/Daftar Akun - Main flow - ATS-14/Script1576653828415.groovy
@@ -17,56 +17,56 @@ import internal.GlobalVariable as GlobalVariable
 
 Mobile.startApplication('C:\\Users\\lieto\\git\\ACC-Seamless\\acc.one (1).apk', false)
 
-Mobile.tap(findTestObject('Daftar Akun/Label Daftar Akun'), 0)
+Mobile.tap(findTestObject('Daftar Akun - ATS-14/Label Daftar Akun'), 0)
 
-Mobile.tap(findTestObject('Daftar Akun/Label Daftar'), 0)
+Mobile.tap(findTestObject('Daftar Akun - ATS-14/Label Daftar'), 0)
 
 if (kontrak.toString() == 'belum') {
-    Mobile.tap(findTestObject('Daftar Akun/Btn Belum Punya Kontrak'), 0)
+    Mobile.tap(findTestObject('Daftar Akun - ATS-14/Btn Belum Punya Kontrak'), 0)
 
-    WebUI.callTestCase(findTestCase('Daftar Akun tests/Daftar Akun'), [('name') : name, ('email') : email, ('phoneNo') : phoneNo
+    WebUI.callTestCase(findTestCase('Daftar Akun tests/Daftar Akun - ATS-14'), [('name') : name, ('email') : email, ('phoneNo') : phoneNo
             , ('radio') : radio, ('otp') : otp, ('password') : password, ('confPassword') : confPassword, ('condition') : condition
             , ('status') : status], FailureHandling.STOP_ON_FAILURE)
 } else {
-    Mobile.tap(findTestObject('Daftar Akun/Btn Punya Kontrak'), 0)
+    Mobile.tap(findTestObject('Daftar Akun - ATS-14/Btn Punya Kontrak'), 0)
 
-    Mobile.setText(findTestObject('Daftar Akun/Field Nopol'), noPol, 0)
+    Mobile.setText(findTestObject('Daftar Akun - ATS-14/Field Nopol'), noPol, 0)
 
-    Mobile.tap(findTestObject('Daftar Akun/Btn Tambah Nopol'), 0)
+    Mobile.tap(findTestObject('Daftar Akun - ATS-14/Btn Tambah Nopol'), 0)
 
     if (status.toString() == 'failed') {
         if (condition.toString() == 'noPolEmpty') {
-            Mobile.verifyElementVisible(findTestObject('Daftar Akun/Warnings/Nomor Polisi Empty'), 0)
+            Mobile.verifyElementVisible(findTestObject('Daftar Akun - ATS-14/Warnings/Nomor Polisi Empty'), 0)
         } else if (condition.toString() == 'noPolNotFound') {
-            Mobile.verifyElementVisible(findTestObject('Daftar Akun/Warnings/Nomor Polisi Not Found'), 0)
+            Mobile.verifyElementVisible(findTestObject('Daftar Akun - ATS-14/Warnings/Nomor Polisi Not Found'), 0)
         }
     } else if (status.toString() == 'success') {
-        Mobile.tap(findTestObject('Daftar Akun/Btn Pick NoTelp'), 0)
+        Mobile.tap(findTestObject('Daftar Akun - ATS-14/Btn Pick NoTelp'), 0)
 
         Mobile.delay(0, FailureHandling.STOP_ON_FAILURE)
 
-        Mobile.setText(findTestObject('Daftar Akun/OTP Nomor Polisi'), '', 0)
+        Mobile.setText(findTestObject('Daftar Akun - ATS-14/OTP Nomor Polisi'), '', 0)
 
-        Mobile.tap(findTestObject('Daftar Akun/Btn Verif OTP NoPol'), 0)
+        Mobile.tap(findTestObject('Daftar Akun - ATS-14/Btn Verif OTP NoPol'), 0)
 
-        Mobile.setText(findTestObject('Daftar Akun/Email Field'), '', 0)
+        Mobile.setText(findTestObject('Daftar Akun - ATS-14/Email Field'), '', 0)
 
         switch (radio.toString()) {
             case 'laki':
-                Mobile.tap(findTestObject('Daftar Akun/Btn Laki-Laki'), 0)
+                Mobile.tap(findTestObject('Daftar Akun - ATS-14/Btn Laki-Laki'), 0)
 
                 break
             case 'perempuan':
-                Mobile.tap(findTestObject('Daftar Akun/Btn Perempuan'), 0)
+                Mobile.tap(findTestObject('Daftar Akun - ATS-14/Btn Perempuan'), 0)
 
                 break
         }
         
-        Mobile.setText(findTestObject('Daftar Akun/Password Field'), '', 0)
+        Mobile.setText(findTestObject('Daftar Akun - ATS-14/Password Field'), '', 0)
 
-        Mobile.setText(findTestObject('Daftar Akun/Confirm Password Field'), '', 0)
+        Mobile.setText(findTestObject('Daftar Akun - ATS-14/Confirm Password Field'), '', 0)
 
-        Mobile.tap(findTestObject('Daftar Akun/Daftar Button'), 0)
+        Mobile.tap(findTestObject('Daftar Akun - ATS-14/Daftar Button'), 0)
     }
 }
 
