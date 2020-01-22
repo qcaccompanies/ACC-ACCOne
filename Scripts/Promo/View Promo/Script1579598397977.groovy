d13828db501074c4a0fdc2c75ce23c6067fd43c4
Rizka Yulianti Pratiwi <rizkariz20@gmail.com>
2020-01-22 16:31:51 +0700

    Rizka Update

---
+++ Scripts/Promo/View Promo/Script1579598397977.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('D:\\MY WORLD\\KULIAH (TUGAS)\\TGS SEMS. 8\\INTERNSHIT\\Task ACC ONE\\ACC-ACCOne\\acc.one.apk', 
+    false)
+
+WebUI.delay(30)
+
+Mobile.swipe(560, 460, 86, 460)
+
+Mobile.tap(findTestObject('Promo/PromoTerbaik'), 0)
+
+Mobile.tap(findTestObject('Promo/BackButton'), 0)
+
+WebUI.delay(5)
+
+Mobile.tap(findTestObject('Promo/LihatSemua'), 0)
+
+WebUI.delay(5)
+
+Mobile.swipe(390, 1100, 390, 600)
+
+Mobile.tap(findTestObject('Promo/BoxPromo'), 0)
+
