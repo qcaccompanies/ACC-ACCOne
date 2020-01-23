a7a6155c9bdd88d304c5c94a4d6b194e3d94e1f7
Rizka Yulianti Pratiwi <rizkariz20@gmail.com>
2020-01-23 16:24:30 +0700

    Update Rizka - Edit Foto Profil

---
+++ Test Suites/Ubah Foto Profil.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Ubah Foto Profil</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f8177414-00db-4999-b7c4-dda9adf8628c</testSuiteGuid>
+   <testCaseLink>
+      <guid>b478f229-5440-45b4-9ec7-02bbff7dd257</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ubah Foto Profil</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>c614b30d-2997-44e1-83c8-163084d9233b</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Ubah Foto Profil</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>c614b30d-2997-44e1-83c8-163084d9233b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>picture</value>
+         <variableId>af8eb27f-509a-428f-a83c-15b4fa44c40f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c614b30d-2997-44e1-83c8-163084d9233b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>namaFoto</value>
+         <variableId>9083e2db-0ad6-42b6-a393-5580fb4d6415</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c614b30d-2997-44e1-83c8-163084d9233b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>takePhoto</value>
+         <variableId>d4b0f890-b9a7-41d4-98df-dc4637d1b591</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c614b30d-2997-44e1-83c8-163084d9233b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>condition</value>
+         <variableId>b808aa7a-a18a-40e6-be83-62e09fdfe500</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
