0bd6b73f0532f0f1fef7a9b0bb2da037ee4d834a
Rizka Yulianti Pratiwi <rizkariz20@gmail.com>
2020-02-11 19:13:59 +0700

    Update Rizka - Lupa Password

---
+++ Scripts/Lupa Kata Sandi/Script1581403879107.groovy
@@ -0,0 +1,116 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('D:\\MY WORLD\\KULIAH (TUGAS)\\TGS SEMS. 8\\INTERNSHIT\\Task ACC ONE\\ACC-ACCOne\\acc.one.apk', 
+    false)
+
+Mobile.delay(20, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Lupa Kata Sandi/txtMasukatauDaftar'), 0)
+
+Mobile.delay(5, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Lupa Kata Sandi/btnLupaKataSandi'), 0)
+
+Mobile.delay(5, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Lupa Kata Sandi/txtEmail'), 0)
+
+Mobile.setText(findTestObject('Lupa Kata Sandi/editTextEmail'), email, 0)
+
+Mobile.tap(findTestObject('Lupa Kata Sandi/btnKonfirmasi'), 0)
+
+Mobile.delay(5, FailureHandling.STOP_ON_FAILURE)
+
+if (kirimOTPke == 'email') {
+    Mobile.tap(findTestObject('Lupa Kata Sandi/kirimKeEmail'), 0)
+} else {
+    Mobile.tap(findTestObject('Lupa Kata Sandi/kirimSMS'), 0)
+}
+
+if (condition == 'kodeOTPsalah') {
+    Mobile.setText(findTestObject('Lupa Kata Sandi/editTextOTP'), OTP, 0)
+
+    Mobile.tap(findTestObject('Lupa Kata Sandi/btnVerifikasi'), 0)
+
+    Mobile.delay(5, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertKodeOTPSalah'), 0)
+} else if (condition == 'tunggu1Menit') {
+    Mobile.delay(5, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.setText(findTestObject('Lupa Kata Sandi/editTextOTP'), OTP, 0)
+
+    Mobile.tap(findTestObject('Lupa Kata Sandi/btnVerifikasi'), 0)
+
+    Mobile.setText(findTestObject('Lupa Kata Sandi/editTextOTP'), OTP, 0)
+
+    Mobile.tap(findTestObject('Lupa Kata Sandi/btnVerifikasi'), 0)
+
+    Mobile.setText(findTestObject('Lupa Kata Sandi/editTextOTP'), OTP, 0)
+
+    Mobile.tap(findTestObject('Lupa Kata Sandi/btnVerifikasi'), 0)
+
+    Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertTunggu1Menit'), 0)
+} else {
+    Mobile.delay(50, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.tap(findTestObject('Lupa Kata Sandi/btnVerifikasi'), 0)
+
+    Mobile.verifyElementNotVisible(findTestObject('Lupa Kata Sandi/alertKodeOTPSalah'), 0)
+
+    Mobile.delay(5, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.setText(findTestObject('Lupa Kata Sandi/editTextSandi1'), kataSandi, 0)
+
+    Mobile.setText(findTestObject('Lupa Kata Sandi/editTextSandi2'), kataSandi2, 0)
+
+    if (condition == 'minimalAda1Angka') {
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertFormatBelumSesuai'), 0)
+
+        Mobile.tap(findTestObject('Lupa Kata Sandi/btnResetDanMasuk'), 0)
+
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertMin1Angka'), 0)
+    } else if (condition == 'minimalAda1Huruf') {
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertFormatBelumSesuai'), 0)
+
+        Mobile.tap(findTestObject('Lupa Kata Sandi/btnResetDanMasuk'), 0)
+
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertMin1Huruf'), 0)
+    } else if (condition == 'passwordHarusDiisi') {
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertPasswordHarusDiisi'), 0)
+
+        Mobile.tap(findTestObject('Lupa Kata Sandi/btnResetDanMasuk'), 0)
+
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertMin7KarFinal'), 0)
+    } else if (condition == 'harus7Karakter') {
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertMin7KarBiasa'), 0)
+
+        Mobile.tap(findTestObject('Lupa Kata Sandi/btnResetDanMasuk'), 0)
+
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertMin7KarFinal'), 0)
+    } else if (condition == 'passwordTidakSesuai') {
+        Mobile.tap(findTestObject('Lupa Kata Sandi/btnResetDanMasuk'), 0)
+
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertPasswordTidakSesuai'), 0)
+    } else if (condition == 'passed') {
+        Mobile.tap(findTestObject('Lupa Kata Sandi/btnResetDanMasuk'), 0)
+
+        Mobile.verifyElementVisible(findTestObject('Lupa Kata Sandi/alertPasswordBerhasilDiganti'), 0)
+    }
+}
+
---
+++ Test Suites/Lupa Kata Sandi.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Lupa Kata Sandi</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>21093da9-355d-48e2-9857-62c3df7330d3</testSuiteGuid>
+   <testCaseLink>
+      <guid>a98ec151-3a71-4b07-a98d-61679ab680ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lupa Kata Sandi</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>c49b8403-29e9-4a42-89ee-4ec4e86eb0e8</id>
+         <iterationEntity>
+            <iterationType>SPECIFIC</iterationType>
+            <value>6</value>
+         </iterationEntity>
+         <testDataId>Data Files/Lupa Kata Sandi</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>c49b8403-29e9-4a42-89ee-4ec4e86eb0e8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>8618a408-f2ec-45b9-a596-3bd14a535701</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c49b8403-29e9-4a42-89ee-4ec4e86eb0e8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>kirimOTPke</value>
+         <variableId>e3d86e84-e565-4f9a-9c4c-04aeb6586898</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c49b8403-29e9-4a42-89ee-4ec4e86eb0e8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>kataSandi</value>
+         <variableId>af3bb755-91d8-45a9-8920-9464234b0464</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c49b8403-29e9-4a42-89ee-4ec4e86eb0e8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>kataSandi2</value>
+         <variableId>b780c593-646f-4c43-9c34-8ecbd577b8ac</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c49b8403-29e9-4a42-89ee-4ec4e86eb0e8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>condition</value>
+         <variableId>967fb37b-e330-4d9b-bdb5-14db094c322a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c49b8403-29e9-4a42-89ee-4ec4e86eb0e8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>OTP</value>
+         <variableId>cfa5fa1e-bba7-4fbe-b7da-af94d7adcbd1</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
