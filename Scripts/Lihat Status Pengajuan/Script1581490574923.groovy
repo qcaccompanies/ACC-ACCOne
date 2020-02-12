bf5c5d0edf257ba23d5d1267a978f68a2a77f390
Rizka Yulianti Pratiwi <rizkariz20@gmail.com>
2020-02-12 15:48:01 +0700

    Update Rizka - Lihat Status Pengajuan

---
+++ Scripts/Lihat Status Pengajuan/Script1581490574923.groovy
@@ -0,0 +1,70 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('D:\\MY WORLD\\KULIAH (TUGAS)\\TGS SEMS. 8\\INTERNSHIT\\Task ACC ONE\\ACC-ACCOne\\acc.one (2).apk', 
+    false)
+
+Mobile.delay(20, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Lihat Status Pengajuan/btnStatusPeng'), 0)
+
+if (condition == 'searchByText') {
+    Mobile.setText(findTestObject('Lihat Status Pengajuan/editTextCariMobilPengajuan'), namaMobil, 0)
+
+    Mobile.delay(10, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.tap(findTestObject('Lihat Status Pengajuan/tapObject', [('text') : namaMobil]), 0)
+
+    Mobile.delay(10, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.tap(findTestObject('Lihat Status Pengajuan/lihatDetailPengajuan'), 0)
+
+    Mobile.delay(10, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.verifyElementVisible(findTestObject('Lihat Status Pengajuan/verifyNomorKontrak'), 0)
+
+    Mobile.delay(5, FailureHandling.STOP_ON_FAILURE)
+} else if (condition == 'searchByScroll') {
+    Mobile.scrollToText(namaMobil, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.delay(10, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.tap(findTestObject('Lihat Status Pengajuan/tapObject', [('text') : namaMobil]), 0)
+
+    Mobile.delay(10, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.tap(findTestObject('Lihat Status Pengajuan/lihatDetailPengajuan'), 0)
+
+    Mobile.delay(10, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.verifyElementVisible(findTestObject('Lihat Status Pengajuan/verifyNomorKontrak'), 0)
+
+    Mobile.delay(5, FailureHandling.STOP_ON_FAILURE)
+} else {
+    Mobile.setText(findTestObject('Lihat Status Pengajuan/editTextCariMobilPengajuan'), namaMobil, 0)
+
+    Mobile.delay(10, FailureHandling.STOP_ON_FAILURE)
+
+    Mobile.verifyElementVisible(findTestObject('Lihat Status Pengajuan/alertPengajuanMobilTidakDitemukan'), 0)
+}
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
---
+++ Test Suites/Lihat Status Pengajuan.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Lihat Status Pengajuan</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>de782562-e844-4367-9d84-c3b399591da2</testSuiteGuid>
+   <testCaseLink>
+      <guid>c7a01bed-7306-483c-b718-f4d58865620c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lihat Status Pengajuan</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>28770811-d400-4616-bb88-0181cf7242d3</id>
+         <iterationEntity>
+            <iterationType>SPECIFIC</iterationType>
+            <value>4</value>
+         </iterationEntity>
+         <testDataId>Data Files/Lihat Status Pengajuan</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>28770811-d400-4616-bb88-0181cf7242d3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>namaMobil</value>
+         <variableId>9f06ddb3-4be4-4713-be8f-80ee08bea308</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>28770811-d400-4616-bb88-0181cf7242d3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>condition</value>
+         <variableId>b708b696-daea-41b9-ae22-6505a8925d87</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
