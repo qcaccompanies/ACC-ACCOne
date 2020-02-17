d8bc7d7eda2ef322c247e5d88b53fd8c71fc9f51
Rizka Yulianti Pratiwi <rizkariz20@gmail.com>
2020-02-17 14:32:24 +0700

    Update Rizka - View Paket

---
+++ Object Repository/View Paket/tapObject.rs
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tapObject</name>
+   <tag></tag>
+   <elementGuidId>3019a309-aaa3-4e56-a8eb-6b47589e320c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'android.view.View' and (contains(text(), '${text}') or contains(., '${text}'))]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>android.view.View</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>${text}</value>
+   </webElementProperties>
+</WebElementEntity>
