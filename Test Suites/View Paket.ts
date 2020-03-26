2c146d846daf2334b3b3e5fdfa2a945eb4e90ad7
Rizka Yulianti Pratiwi <rizkariz20@gmail.com>
2020-03-26 17:31:02 +0700

    Update Rizka - Lihat Paket

---
+++ Test Suites/View Paket.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>View Paket</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c184acbe-ec44-4837-8ce5-55196d4a387a</testSuiteGuid>
+   <testCaseLink>
+      <guid>0f20e356-9e6b-4acb-88be-4ea86d5891c0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/View Paket</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>1d568ca9-858d-4725-b17b-5ed456dcb29c</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/viewPaket</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>1d568ca9-858d-4725-b17b-5ed456dcb29c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>namaPaket</value>
+         <variableId>abd87c60-d109-408d-9744-b67951b4c838</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>1d568ca9-858d-4725-b17b-5ed456dcb29c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>condition</value>
+         <variableId>f5edcb9c-ac4c-49f9-bcf8-bd08d7e6a3bd</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
